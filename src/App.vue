4f323864b4a4be0e7b153af4ee07a746de76553d
Sojeong Park <macpark09@gmail.com>
2021-12-16 15:46:32 +0000

    Hero component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
 <div class="app">
-  Hello world again
+  
+ <Hero />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Hero from './components/Hero.vue'
 
 export default defineComponent({
   name: 'App',
-  components: {
-   
-  }
+  components: {Hero},
+  data() {
+    return {
+      visible:  true
+      }
+    },
+   methods: {
+     updateVisible() {
+      this.visible=true
+      
+        } 
+    } 
+  
+  
 });
 </script>
 
---
+++ src/components/Hero.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="front" >
+
+        <img :src="image">
+        <p class="lead">{{leadText}}</p>
+        <p class="title">{{title}}</p>
+        <p class="description">{{desciption}}</p>
+
+    <div class="button">
+        <button 
+        class="sub-button"
+        v-on:click="changeVisible"
+        >NEW ARRIVALS</button>
+        <button 
+        class="sub-button"
+        v-on:click="changeVisible"
+        >BEST SELLERS</button>
+    </div>
+
+    </div>
+    
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    /* props: {
+     change: {
+         type: Boolean,
+         required: true
+        }
+    },  */
+     data() {
+    return {
+      image: '../assets/images/diamond.jpg',
+      leadText: 'DISCOVER',
+      title: 'YOUR STONE',
+      desciption: 'Start your engagement ring journey here.Craft your own bespoke design or browse from our ready to wear collection',
+      visible: true 
+      }
+    },
+    methods: {
+     changeVisible() {
+       this.$emit('change-visible')
+        } 
+    } 
+   
+})
+</script>
+
+<style>
+.front {
+  
+  text-align: center;
+
+    } 
+.lead {
+    font-size: 20px;
+    }
+.title {
+    font-size: 28px;
+    }
+.description {
+    font-size: 18px;
+    }
+.sub-button {
+  margin: 20px;
+  background-color: #39495c;
+  border-radius: 5px;
+  color: white;
+  padding: 10px;
+  text-align: center;
+  cursor: pointer;
+}
+</style>
